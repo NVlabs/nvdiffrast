Fix rotate_x sign bug in samples/torch/util.py

samples/torch/cube.py
@@ -95,7 +95,7 @@ def fit_cube(max_iter          = 5000,
             r_rot = util.random_rotation_translation(0.25)
 
             # Smooth rotation for display.
-            a_rot = np.matmul(util.rotate_x(-0.4), util.rotate_y(ang))
+            a_rot = np.matmul(util.rotate_x(0.4), util.rotate_y(ang))
 
             # Modelview and modelview + projection matrices.
             proj  = util.projection(x=0.4)

samples/torch/earth.py
@@ -101,7 +101,7 @@ def fit_earth(max_iter          = 20000,
         r_rot = util.random_rotation_translation(0.25)
 
         # Smooth rotation for display.
-        a_rot = np.matmul(util.rotate_x(-0.4), util.rotate_y(ang))
+        a_rot = np.matmul(util.rotate_x(0.4), util.rotate_y(ang))
         dist = np.random.uniform(0.0, 48.5)
 
         # Modelview and modelview + projection matrices.

samples/torch/envphong.py
@@ -89,7 +89,7 @@ def fit_env_phong(max_iter          = 1000,
 
         # Smooth rotation for display.
         ang = ang + 0.01
-        a_rot = np.matmul(util.rotate_x(-0.4), util.rotate_y(ang))
+        a_rot = np.matmul(util.rotate_x(0.4), util.rotate_y(ang))
 
         # Modelview and modelview + projection matrices.
         proj  = util.projection(x=0.4, n=1.0, f=200.0)

samples/torch/util.py
@@ -27,10 +27,10 @@ def translate(x, y, z):
 
 def rotate_x(a):
     s, c = np.sin(a), np.cos(a)
-    return np.array([[1,  0, 0, 0],
-                     [0,  c, s, 0],
-                     [0, -s, c, 0],
-                     [0,  0, 0, 1]]).astype(np.float32)
+    return np.array([[1, 0,  0, 0],
+                     [0, c, -s, 0],
+                     [0, s,  c, 0],
+                     [0, 0,  0, 1]]).astype(np.float32)
 
 def rotate_y(a):
     s, c = np.sin(a), np.cos(a)